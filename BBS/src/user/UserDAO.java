c7272c9dfe16339ef26abceab86700fb9183ba81
noose <hd15807@gmail.com>
2020-11-18 01:11:51 +0900

    Add login function

---
+++ BBS/src/user/UserDAO.java
@@ -0,0 +1,47 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+/*database access object*/
+public class UserDAO {
+	private Connection conn; //
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.jdbc.Driver"); //driver
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			//pstmt sql injection 방어 
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword))
+					return 1; // login 성공
+				else
+					return 0; // password 불일치
+			}
+			return -1; // no ID
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //DB error code
+	}
+}
